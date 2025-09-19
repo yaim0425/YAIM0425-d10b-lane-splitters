0821deb7fa68622bf2482f7f53feeee750f0335b
yaim0425 <yaim0425@gmail.com>
2025-09-18 19:21:51 -0500

    Creación de data-final-fixes.lua

---
+++ data-final-fixes.lua
@@ -0,0 +1,113 @@
+---------------------------------------------------------------------------
+---[ data-final-fixes.lua ]---
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Contenedor de este archivo ]---
+---------------------------------------------------------------------------
+
+local This_MOD = GMOD.get_id_and_name()
+if not This_MOD then return end
+GMOD[This_MOD.id] = This_MOD
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Inicio del MOD ]---
+---------------------------------------------------------------------------
+
+function This_MOD.start()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Valores de la referencia
+    This_MOD.setting_mod()
+
+    -- --- Obtener los elementos
+    -- This_MOD.get_elements()
+
+    -- --- Modificar los elementos
+    -- for iKey, spaces in pairs(This_MOD.to_be_processed) do
+    --     for jKey, space in pairs(spaces) do
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         --- Marcar como procesado
+    --         This_MOD.processed[iKey] = This_MOD.processed[iKey] or {}
+    --         This_MOD.processed[iKey][jKey] = true
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         --- Crear los elementos
+    --         This_MOD.create_recipe(space)
+    --         This_MOD.create_item(space)
+    --         This_MOD.create_entity(space)
+    --         This_MOD.create_tech(space)
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --     end
+    -- end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Valores de la referencia ]---
+---------------------------------------------------------------------------
+
+function This_MOD.setting_mod()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Validar si se cargó antes
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    if This_MOD.processed then return end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en todos los MODs
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Contenedor de los elementos que el MOD modoficó o modificará
+    This_MOD.to_be_processed = {}
+    This_MOD.processed = {}
+
+    --- Cargar la configuración
+    This_MOD.setting = GMOD.setting[This_MOD.id]
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en este MOD
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Tiempo de creación de las recetas
+    This_MOD.time = This_MOD.setting.time
+    --- Min. 1 (1s)
+    --- Max. 65000 (18h)
+    --- Def. 300 (5m)
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
