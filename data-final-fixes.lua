2ffac1be9593dab57b15929318f32b6c923910e8
yaim0425 <yaim0425@gmail.com>
2025-10-10 20:22:42 -0500

    Cambio en la actualización de Entity.next_upgrade

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -347,14 +347,14 @@ function This_MOD.create_entity(space)
     } }
 
     --- Siguiente tier
-    Entity.next_upgrade = (function()
+    Entity.next_upgrade = (function(entity)
         --- Validación
-        if not Entity.next_upgrade then return end
+        if not entity then return end
 
         --- Procesar el nombre
         local That_MOD =
-            GMOD.get_id_and_name(Entity.next_upgrade) or
-            { ids = "-", name = Entity.next_upgrade }
+            GMOD.get_id_and_name(entity) or
+            { ids = "-", name = entity }
 
         --- Nombre despues del aplicar el MOD
         local Name =
@@ -370,12 +370,12 @@ function This_MOD.create_entity(space)
         --- La entidad existirá
         for _, Spaces in pairs(This_MOD.to_be_processed) do
             for _, Space in pairs(Spaces) do
-                if Space.entity.name == Entity.next_upgrade then
+                if Space.entity.name == entity then
                     return Name
                 end
             end
         end
-    end)()
+    end)(Entity.next_upgrade)
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
